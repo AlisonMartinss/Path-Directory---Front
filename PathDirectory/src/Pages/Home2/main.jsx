fad67a50ea3a71edc393fc35e921d4b98f68a393
Alison Martins <alisonmarteso@gmail.com>
2024-09-18 04:37:11 -0300

    lidando com fontes - ultima da madrugada

--- PathDirectory/src/Pages/Home2/main.jsx
+++ PathDirectory/src/Pages/Home2/main.jsx
@@ -11,8 +11,8 @@ return(
 <div className="backCarrosel">
     <div className="carrosel"><SlideShow/></div>
     <div className="slogan">
-      <h4>Guie seus primeiros passos <strong>com quem já trilhou o caminho.</strong></h4>
-      <h4>Playlists,guias e insigths para sua jornada profissional.</h4>
+      <h4 className='slogantxt'>Guie seus primeiros passos <strong>com quem já trilhou o caminho.</strong></h4>
+      <h4 className='slogantxt'>Playlists,guias e insigths para sua jornada profissional.</h4>
     </div>
     <div >
       <img className="trilha" src="../../../public/Midias/trilha02.png" alt="icone de trilha" />
@@ -22,7 +22,7 @@ return(
     </div> 
 </div>
 <div className='orientacao'>
-  
+
   <h3 className='orientacao-filho'>Assista também</h3>
 
   <div className='videos'>
@@ -41,15 +41,15 @@ return(
 
     <h3 className='Qsomos_filho'>Quem Somos</h3>
     <div className='txt01'>
-      <h4>Somos uma plataforma colaborativa voltada para o meio educativo, onde profissionais e estudantes
+      <h4 className='slogantxt'>Somos uma plataforma colaborativa voltada para o meio educativo, onde profissionais e estudantes
       criam perfis e compartilham playlists e roadmaps</h4>
-      <h4>com conteúdo acadêmico de qualidade.</h4>
+      <h4 className='slogantxt'>com conteúdo acadêmico de qualidade.</h4>
     </div>
     
     <div className='txt02'>
-      <h4>Com essas playlists, criadas por pessoas do meio ao qual o conteúdo se refere, você pode otimizar 
+      <h4 className='txt02_filho'>Com essas playlists, criadas por pessoas do meio ao qual o conteúdo se refere, você pode otimizar 
         seu tempo e sua produtividade,além de se poupar de fazer</h4>
-      <h4>buscas que levariam horas para encontrar um conteúdo de qualidade.</h4>
+      <h4 className='txt02_filho'>buscas que levariam horas para encontrar um conteúdo de qualidade.</h4>
       <h4></h4>
     </div>
 
