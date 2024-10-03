585d4cfa987135437fd44e6f663675abd51191b5
Alison Martins <alisonmarteso@gmail.com>
2024-10-03 00:45:41 -0300

    Trabalhando no Loby

--- PathDirectory/src/Pages/Loby/Loby.jsx
+++ PathDirectory/src/Pages/Loby/Loby.jsx
@@ -2,6 +2,8 @@ import Home2 from "../Home2/Home2";
 import Porcentagem from "../FunçõesGlobais/Barra Porcentagem/Porcentagem";
 import CabecalhoPesquisa from "../FunçõesGlobais/CabecalhoPesquisa/CabecalhoPesquisa.jsx"
 import '../Loby/Loby.css'
+import '../../../public/Midias/Lapis-Edit.png'
+
 
 function Loby () {
     return(
@@ -17,16 +19,19 @@ function Loby () {
 
                 <div className="listagem">
                     <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
                         <div className="curso_filho">Nome Do Curso 1</div>
                         <Porcentagem/>
                     </div>
 
                     <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
                         <div className="curso_filho">Nome Do Curso 1</div>
                         <Porcentagem/>
                     </div>
 
                     <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
                         <div className="curso_filho">Nome Do Curso 1</div>
                         <Porcentagem/>
                     </div>
@@ -37,14 +42,49 @@ function Loby () {
                         <div>
                            <img className="fotoperfil" src="https://github.com/AlisonMartinss/Assets/blob/main/Captura%20de%20tela%202024-06-19%20141014.png?raw=true" alt="" />
                         </div>
+
                         <div className="info">
-                            Credencial: Academica
+
+                          <div className="info_jr">
+                            Nome: Alison
+                          </div>
+
+                          <div className="editar_info">
+                            <img className="icon"  src="../../../public/Midias/Lapis-Edit.png" alt="" />
+                          </div>
+
                         </div>
-                        <div  className="info">
-                            Nome: Sivirino
+
+                        <div className="info">
+
+                            <div className="info_jr">
+                                Credencial: Academica
+                            </div>
+
+                            <div className="editar_info">
+                                <img className="icon"  src="../../../public/Midias/Lapis-Edit.png" alt="" />
+                            </div>
+
                         </div>
-                        <div  className="info">
-                            Path Criados 
+
+
+
+
+    
+                      
+                        <div  className="redes_sociais">
+                            <div className="lattes rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
+                            <div className="linkedim rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
+                            <div className="github rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
                         </div>
                     </div>
 
