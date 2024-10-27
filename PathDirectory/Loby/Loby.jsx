5f1086f7d0c96d9713469b1d061cca5cb7bd79b8
Alison Martins <alisonmarteso@gmail.com>
2024-10-27 01:42:12 -0300

    Trabalhando no Path de Criação

---
+++ PathDirectory/Loby/Loby.jsx
@@ -0,0 +1,95 @@
+import Home2 from "../Home2/Home2";
+import Porcentagem from "../FunçõesGlobais/Barra Porcentagem/Porcentagem";
+import '../Loby/Loby.css'
+import '../../../public/Midias/Lapis-Edit.png'
+import '../../../public/Midias/lupa.png'
+
+
+function Loby () {
+    return(
+    <div className="all">
+        <header className="header_loby"><Home2/></header>
+    
+        <main className="main">
+
+                <div className="listagem">
+                    <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
+                        <div className="curso_filho">Nome Do Curso 1</div>
+                        <Porcentagem/>
+                    </div>
+
+                    <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
+                        <div className="curso_filho">Nome Do Curso 1</div>
+                        <Porcentagem/>
+                    </div>
+
+                    <div className="curso">
+                        <div className="fav_icon">favoritismo</div>
+                        <div className="curso_filho">Nome Do Curso 1</div>
+                        <Porcentagem/>
+                    </div>
+                </div>  
+
+                <div className="perfil">
+                    <div className="perfil_jr">
+                        <div>
+                           <img className="fotoperfil" src="https://github.com/AlisonMartinss/Assets/blob/main/Captura%20de%20tela%202024-06-19%20141014.png?raw=true" alt="" />
+                        </div>
+
+                        <div className="info">
+
+                          <div className="info_jr">
+                            Nome: Alison
+                          </div>
+
+                          <div className="editar_info">
+                            <img className="icon"  src="../../../public/Midias/Lapis-Edit.png" alt="" />
+                          </div>
+
+                        </div>
+
+                        <div className="info">
+
+                            <div className="info_jr">
+                                Credencial: Academica
+                            </div>
+
+                            <div className="editar_info">
+                                <img className="icon"  src="../../../public/Midias/Lapis-Edit.png" alt="" />
+                            </div>
+
+                        </div>
+
+
+
+
+    
+                      
+                        <div  className="redes_sociais">
+                            <div className="lattes rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
+                            <div className="linkedim rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
+                            <div className="github rede_jr">
+                                <img className="icon" src="../../../public/Midias/Logo-Ref-Linkedim.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+        </main>
+    </div> 
+
+    )
+}
+
+export default Loby
--- PathDirectory/src/Pages/Path/Path.jsx
+++ PathDirectory/src/Pages/Path/Path.jsx
@@ -1,20 +1,24 @@
 import '../Path/Path.css'
+import Botao from '../FunçõesGlobais/Botão/Botao';
 
 function Path () {
 return (
 <main className="all_Path">
     <div className='formulario'>
         <form className='formulario_jr' action="">
-          <input className="input_Path_titulo"  type="text" id="user" placeholder="Digite O Titulo do Modulo" required
-          maxlength="25" minlength="15"/>
+                <div className="input_titulo_modulo">
+                    <input className='input_titulo_modulo_jr' type="text" id="user" placeholder="Digite O Titulo do Modulo" required
+                    maxlength="25" minlength="15"/>
+                </div>
+                
 
                 <div className='formulario_aula_titulo'>   
                   <input className="input_titulo"  type="text" id="user" placeholder="Digite O Titulo da Aula" required
                   maxlength="25" minlength="15"/>
 
                   <div className='label_Path'>
-                  <label for="numero"><h6>Selecione o numero correpondente</h6><h6>a colocação da aula:</h6></label>
-                    <select id="numero">
+                  <label for="numero"><div>Selecione o numero correpondente</div><div>a colocação da aula:</div></label>
+                    <select className='caixaOPnumero' id="numero">
                         <option value="10">1</option>
                         <option value="20">2</option>
                         <option value="30">3</option>
@@ -41,37 +45,41 @@ return (
                     <input className="input_Path_link input_titulo"  type="text" id="user" placeholder="Digite o link do conteudo" required
                     maxlength="25" minlength="15"/>
 
-                <div className='label_Path'>
+                    <div className='label_Path'>
 
-                <label className='label_Path' for="numero">Selecione formato do conteudo:</label>
-                    <select id="numero">
-                        <option value="1">
-                        <div>
-                            <h5> Video No YouTube:</h5>
-                            <h6> O video que você pretende colocar está no YouTube.</h6>
-                        </div>
-                        </option>
-
-
-                        <option value="2">
-
-                        <div>
-                            <h5> Outro:</h5>
-                            <h6> O conteudo que você pretende colocar não está no YouTube.</h6>
-                        </div>
-
-                        </option>
-                        
-                    </select>
-                </div>    
+                            <label className='label_Path_' for="numero">Selecione formato do conteudo:</label>
+                            <select className='SelectStyle formatoConteudo' id="numero">
+                                <option value="1">
+                                <div>
+                                    <h5> Video No YouTube:</h5>
+                                    <h6> O video que você pretende colocar está no YouTube.</h6>
+                                </div>
+                                </option>
 
 
-             
+                                <option value="2">
+
+                                <div>
+                                    <h5> Outro:</h5>
+                                    <h6> O conteudo que você pretende colocar não está no YouTube.</h6>
+                                </div>
+
+                                </option>
+                                
+                            </select>
+                    </div>
                 </div>
-                
-                
-                <input className="input_Path_desc"  type="text" id="user" placeholder="Adicione a descriçaõ da aula" required
-                maxlength="25" minlength="15"/>
+
+                <div className='descricao'>
+                    <textarea className='input_Path_desc' id="description" name="description"
+                    rows="4" cols="50" placeholder="Digite a descrição da aula..."></textarea>
+                </div>
+
+                <div className='acoes'>
+                    <Botao texto={"Salvar"}/>
+                    <input className='acoes_definitivas b_enviar' type="submit" value="Enviar"/>
+                    <input className='acoes_definitivas b_salvar' type="submit" value="Salvar"/>
+                </div>
         </form>
     </div>
 </main>
