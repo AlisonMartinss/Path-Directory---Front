9f9460260b7ad05ddb7451d92978c57ef3742fdc
Alison Martins <alisonmarteso@gmail.com>
2024-10-28 17:43:13 -0300

    Trabalhando no form do Path

---
+++ PathDirectory/src/Pages/FunçõesGlobais/CaixaInput/CaixaInput.jsx
@@ -0,0 +1,12 @@
+function CaixaInput () {
+    return (
+    <select id="opcoes" name="opcoes">
+        <option value="opcao1">Opção 1</option>
+        <option value="opcao2">Opção 2</option>
+        <option value="opcao3">Opção 3</option>
+    </select>
+
+    )
+}
+
+export default CaixaInput;
--- PathDirectory/src/Pages/Path/Path.jsx
+++ PathDirectory/src/Pages/Path/Path.jsx
@@ -1,93 +1,47 @@
 import '../Path/Path.css'
-import Botao from '../FunçõesGlobais/Botão/Botao';
+import Home2 from "../Home2/Home2";
+import CaixaInput from '../FunçõesGlobais/CaixaInput/CaixaInput';
+import '../../../public/Midias/etapas-img.png'
 
 function Path () {
 return (
 <main className="all_Path">
-    <div className='formulario'>
-        <form className='formulario_jr' action="">
-                <div className="input_titulo_modulo">
-                    <input className='input_titulo_modulo_jr' type="text" id="user" placeholder="Digite O Titulo do Modulo" required
-                    maxlength="25" minlength="15"/>
-                </div>
-                
 
-                <div className='formulario_aula_titulo'>   
-                  <input className="input_titulo"  type="text" id="user" placeholder="Digite O Titulo da Aula" required
-                  maxlength="25" minlength="15"/>
+   <header className='header_Path'>
+          <div className='etapas'>
+            <img className='etapas-img' src="../../../public/Midias/etapas-img.png" alt="" />
+          </div>
 
-                  <div className='label_Path'>
-                  <label for="numero"><div>Selecione o numero correpondente</div><div>a colocação da aula:</div></label>
-                    <select className='caixaOPnumero' id="numero">
-                        <option value="10">1</option>
-                        <option value="20">2</option>
-                        <option value="30">3</option>
-                        <option value="40">4</option>
-                        <option value="50">5</option>
+          <div className='textos_CTA'>
+            <div className='textos_CTA_tx txttA'>Criar Path</div>
+            <div className='textos_CTA_tx txttB'>Criar Modulo</div>
+          </div>
 
-                        <option value="10">6</option>
-                        <option value="20">7</option>
-                        <option value="30">8</option>
-                        <option value="40">9</option>
-                        <option value="50">10</option>
+          <div className='home_contanier'><Home2/></div>
+    </header>
 
-                        <option value="10">11</option>
-                        <option value="20">12</option>
-                        <option value="30">13</option>
-                        <option value="40">14</option>
-                        <option value="50">15</option>
-                    </select>
-                  </div>
+    <div className='central'>
+        <form className='form_Path' action="">
+            <input className='inside_input_Path' type="text" id="nome" name="nome" placeholder="Digite o nome do seu Path"/>
 
-                </div>
+            <div className='CTO'>
+                Selecione cinco <strong className='stg_Path'>adjetivos</strong> que melhor descrevem o seu Path:
+            </div>
 
-                <div className='formulario_link'>
-                    <input className="input_Path_link input_titulo"  type="text" id="user" placeholder="Digite o link do conteudo" required
-                    maxlength="25" minlength="15"/>
-
-                    <div className='label_Path'>
-
-                            <label className='label_Path_' for="numero">Selecione formato do conteudo:</label>
-                            <select className='SelectStyle formatoConteudo' id="numero">
-                                <option value="1">
-                                <div>
-                                    <h5> Video No YouTube:</h5>
-                                    <h6> O video que você pretende colocar está no YouTube.</h6>
-                                </div>
-                                </option>
-
-
-                                <option value="2">
-
-                                <div>
-                                    <h5> Outro:</h5>
-                                    <h6> O conteudo que você pretende colocar não está no YouTube.</h6>
-                                </div>
-
-                                </option>
-                                
-                            </select>
-                    </div>
-                </div>
-
-                <div className='descricao'>
-                    <textarea className='input_Path_desc' id="description" name="description"
-                    rows="4" cols="50" placeholder="Digite a descrição da aula..."></textarea>
-                </div>
-
-                <div className='acoes'>
-                    <Botao texto={"Salvar"}/>
-                    <input className='acoes_definitivas b_enviar' type="submit" value="Enviar"/>
-                    <input className='acoes_definitivas b_salvar' type="submit" value="Salvar"/>
-                </div>
+            <div className='adjetivos_Path'>
+              <CaixaInput/>
+              <CaixaInput/>
+              <CaixaInput/>
+              <CaixaInput/>
+              <CaixaInput/>
+            </div>
         </form>
+
     </div>
+
+   
 </main>
-
-
-
-
-)
+ )
 } 
 
 export default Path;
