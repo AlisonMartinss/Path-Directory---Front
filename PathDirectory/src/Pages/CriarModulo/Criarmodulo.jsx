767108fdbc467557888bc82d79ea776ea8312faa
Alison Martins <alisonmarteso@gmail.com>
2024-11-05 22:06:58 -0300

    Criando aba de Modulo e conteudo

--- PathDirectory/src/Pages/CriarModulo/Criarmodulo.jsx
+++ PathDirectory/src/Pages/CriarModulo/Criarmodulo.jsx
@@ -4,7 +4,7 @@ import Home2 from '../Home2/Home2';
 
 function Criarmodulo () {
     return (
-        <main className='main_modulo'>
+        <div className='main_modulo'>
             <header className='header_Path'>
                 <div className='etapas'>
                     <img className='etapas-img' src="../../../public/Midias/etapas-img.png" alt="" />
@@ -17,18 +17,31 @@ function Criarmodulo () {
 
                 <div className='home_contanier'><Home2/></div>
             </header>
-           <div className='modulo_input'>
-                <form action="">
-                    <div className='input_Path'>
-                    <input className='inside_input_Path' type="text" id="nome" name="nome" placeholder="Digite o nome do modulo"/>
+
+            <main className='CrMo-main'>
+                <div className='modulo_input'>
+                    <div className='CrMo-title'>
+                        Modulo
                     </div>
 
-                    <div className='descricao'>
-                    <textarea className='descricao_area' placeholder='Descreva seu Path . . .' id="description" name="description" rows="4" cols="50" maxlength="3000"></textarea>
-                    </div>
-                </form>
-           </div>
-        </main>
+                    <form className='CrMo-form' action="">
+                        <div className='CrMo-input_Path'>
+                        <input className='inside_input_Path' type="text" id="nome" name="nome" placeholder="Digite o nome do modulo"/>
+                        </div>
+
+                        <div className='CrMo-descricao'>
+                        <textarea className='descricao_area' placeholder='Descreva seu Path . . .' id="description" name="description" rows="4" cols="50" maxlength="3000"></textarea>
+                        </div>
+                    </form>
+                </div>
+                
+                <div className='CrMo-curso_input'>
+
+                </div>
+            </main>
+
+   
+        </div>
 
     )
 }
