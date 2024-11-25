4ce9fdcf04e1792fc3d4d581709eefe6cc562145
Alison Martins <alisonmarteso@gmail.com>
2024-11-25 14:36:36 -0300

    Finalizando a area de acesso ao Path

--- PathDirectory/src/Pages/ConteudoPath/ConteudoPath.jsx
+++ PathDirectory/src/Pages/ConteudoPath/ConteudoPath.jsx
@@ -42,8 +42,16 @@ function ConteudoPath () {
 
                     </div>
 
+                    <h4 className={`${styles.h4}`}>Sobre o <strong className={styles.strong}>Path:</strong></h4>
+
                     <div className={styles.desc_desc}>
-                        <div className={styles.adjetivos}></div>
+                        <div className={styles.adjetivos}>
+                            <div className={`${styles.adjetivos_carac} ${styles.adjetivo01}`}>Adjetivo 1</div>
+                            <div className={`${styles.adjetivos_carac} ${styles.adjetivo02}`}>Adjetivo 2</div>
+                            <div className={`${styles.adjetivos_carac} ${styles.adjetivo03}`}>Adjetivo 3</div>
+                            <div className={`${styles.adjetivos_carac} ${styles.adjetivo04}`}>Adjetivo 4</div>
+                            <div className={`${styles.adjetivos_carac} ${styles.adjetivo05}`}>Adjetivo 5</div>
+                        </div>
                         <div className={`${styles.desc_Descritivo} ${styles.txtV2}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
