de82832ba2a91eba0e1a95ea9e64c30df2c55b26
Alison Martins <alisonmarteso@gmail.com>
2024-11-29 13:02:39 -0300

    Apresentação do conteudo finalizado

--- PathDirectory/src/Pages/Curso/Curso.jsx
+++ PathDirectory/src/Pages/Curso/Curso.jsx
@@ -1,42 +1,97 @@
-import '../Curso/curso.css'
+import styles from '../Curso/curso.module.css'
 
 function Curso () {
     return(
       
         <main>
-        <aside class="lateral">
-          <div className='lateral_titulacao'>
-         
-             <h4>TITULO HIPOTETICO</h4>
           
-            
-          </div>
-          <div className='lateral_conteudo_listagem'>
+          <aside class={styles.lateral}>
+            <div className={styles.titulo}>
+              <h4>TITULO HIPOTETICO</h4>
+            </div>
+            <div className={styles.lista}>
+              <div className={styles.aula}>
 
-           <div className='lateral_conteudo'>TITULO 01</div>
-           <div className='lateral_conteudo'>TITULO 02</div>
-           <div className='lateral_conteudo'>TITULO 03</div>
-           <div className='lateral_conteudo'>TITULO 04</div>
-           <div className='lateral_conteudo'>TITULO 05</div>
-           <div className='lateral_conteudo'>TITULO 06</div>
-           <div className='lateral_conteudo'>TITULO 07</div>
-           <div className='lateral_conteudo'>TITULO 08</div>
-           <div className='lateral_conteudo'>TITULO 09</div>
-           <div className='lateral_conteudo'>TITULO 10</div>
-           <div className='lateral_conteudo'>TITULO 11</div>
-           <div className='lateral_conteudo'>TITULO 12</div>
-           <div className='lateral_conteudo'>TITULO 13</div>
-           <div className='lateral_conteudo'>TITULO 14</div>
-           <div className='lateral_conteudo'>TITULO 15</div>
-           
-          </div>
-        </aside>
-        <div class="conteudo">
-        <iframe className='conteudo_video' src="https://www.youtube.com/embed/AtK2m5ROzQM" frameborder="0"></iframe>
-        <div className='conteudo_adicional'>
-          
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+              
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+
+              <div className={styles.aula}>
+
+              </div>
+            </div>
+
+          </aside>
+        <div class={styles.conteudo}>
+           <iframe className={styles.conteudo_video} src="https://www.youtube.com/embed/AtK2m5ROzQM" frameborder="0"></iframe>
+
+
+          <div className={styles.descricao}>
+             <p className={styles.txtG}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+              in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+              
+             </p>
             
-        </div>
+              
+          </div>
 
 
         </div>
